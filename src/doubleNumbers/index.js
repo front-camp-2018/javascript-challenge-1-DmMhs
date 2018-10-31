61a8d514c16b394bfd915eb3b727de8389752fd5
Dm Mh <dmmhs888@gmail.com>
2018-10-31 17:29:36 +0200

    Add new check

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,9 +1,9 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  const str  = num + '';
+  const str  = `${num}`;
   const leftSubStr = str.slice(0, str.length / 2);
   const rightSubStr = str.slice(str.length / 2, str.length);
-  if (str.length > 1 && leftSubStr === rightSubStr ) {
+  if (str.length > 1 && str.length % 2 === 0 && leftSubStr === rightSubStr ) {
     return num;
   } else {
     return num * 2;
